988729b05304674f1a35e1da7fa80304a5e57c89
Wagner Galvao <galvao.wagner@outlook.com.br>
2021-06-01 18:33:15 -0300

    Atualizado em 01/06/2021 18:33:02

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -6,6 +6,10 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Locale;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.github.javafaker.Faker;
@@ -15,34 +19,54 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
 
-	ReajusteService service = new ReajusteService();
+	ReajusteService service;
 	Funcionario funcionario;
 
 	private Faker fake = new Faker(new Locale("pt-BR"));
 
+	@BeforeAll
+	public static void antesDeTodos() {
+		System.out.println("ANTES DE TODOS");
+	}
+
+	@BeforeEach
+	public void inicializar() {
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario(fake.name().name(), LocalDate.now().minusDays(fake.random().nextInt(365)),
+				new BigDecimal("1000.00"));
+		System.out.println(
+				funcionario.getNome() + " - " + funcionario.getDataAdmissao() + " - " + funcionario.getSalario());
+	}
+
+	@AfterEach
+	public void finalizar() {
+		System.out.println(
+				funcionario.getNome() + " - " + funcionario.getDataAdmissao() + " - " + funcionario.getSalario());
+		System.out.println("");
+	}
+
+	@AfterAll
+	public static void depoisDeTodos() {
+		System.out.println("DEPOIS DE TODOS");
+	}
+
 	@Test
 	public void deveRetornarReajusteDeTresPorcentoParaDesemepenhoADesejar() {
-		funcionario = new Funcionario(fake.name().name(),
-				LocalDate.now().minusDays(fake.random().nextInt(365)), new BigDecimal("1000.00"));
 		service.calcularDesempenho(funcionario, Desempenho.A_DESEJAR);
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
 
 	@Test
-	public void deveRetornarReajusteDeTresPorcentoParaDesemepenhoBom() {
+	public void deveRetornarReajusteDeQuizePorcentoParaDesemepenhoBom() {
 		service = new ReajusteService();
-		funcionario = new Funcionario(fake.name().name(),
-				LocalDate.now().minusDays(fake.random().nextInt(365)), new BigDecimal("2000.00"));
 		service.calcularDesempenho(funcionario, Desempenho.BOM);
-		assertEquals(new BigDecimal("2300.00"), funcionario.getSalario());
+		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
 
 	@Test
-	public void deveRetornarReajusteDeTresPorcentoParaDesemepenhoOtimo() {
+	public void deveRetornarReajusteDeVintePorcentoParaDesemepenhoOtimo() {
 		service = new ReajusteService();
-		funcionario = new Funcionario(fake.name().name(),
-				LocalDate.now().minusDays(fake.random().nextInt(365)), new BigDecimal("3000.00"));
 		service.calcularDesempenho(funcionario, Desempenho.OTIMO);
-		assertEquals(new BigDecimal("3600.00"), funcionario.getSalario());
+		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
 }
